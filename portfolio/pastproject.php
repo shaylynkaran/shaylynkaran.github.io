b7dd3084272f36ec6dc02f41c2db2eefd46aa207
shaylynkaran <shaylynskaran@gmail.com>
2018-11-16 15:42:54 -0800

    updated nav

--- portfolio/pastproject.php
+++ portfolio/pastproject.php
@@ -20,8 +20,18 @@
   <a href="index.php" class="logo">Shaylyn's Portfolio</a>
   <input class="menu-btn" type="checkbox" id="menu-btn" />
   <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
-
-  <?php include 'includes/nav.php'; ?>
+  <!-- BEGIN NAVBAR -->
+  <nav>
+    <a href="currentproject.php">Current Project(s)</a>
+    <a href="pastproject.php">Past Projects</a>
+    <a href="experience.php">Experience</a>
+    <a href="education.php">Education</a>
+    <a href="skills.php">Skills</a>
+    <a href="blog.php">Blog Posts</a>
+    <a href="about.php">About Me</a>
+    <a href="contact.php">Contact</a>
+  </nav>
+  <!-- END NAVBAR -->
 </header>
 <main>
     <h1>Previous Projects</h1>
