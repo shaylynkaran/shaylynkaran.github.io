36742d021ca4845f7a6db9df2caf0b30e2f0cc6b
shaylynkaran <shaylynskaran@gmail.com>
2018-11-16 15:37:28 -0800

    updated header

--- portfolio/blog.php
+++ portfolio/blog.php
@@ -1,12 +1,28 @@
-<?php
-    // Define variables used in the HTML header for this page.
-    $title = 'Shaylyn Karan | Blog' ;
-    $description = 'Blog post for portfolio' ;
-    $author = 'Shaylyn Karan' ;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    // Display the header which uses the above variables.
-    include 'includes/header.php' ;
-?>
+    <title>Blog | Shaylyn Karan></title>
+    <meta name="description" content="Personal Portfolio">
+    <meta name="author" content="Shaylyn Karan">
+
+    <!-- link to external CSS file -->
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+<header class="header">
+  <a href="index.php" class="logo">Shaylyn's Portfolio</a>
+  <input class="menu-btn" type="checkbox" id="menu-btn" />
+  <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+
+  <?php include 'includes/nav.php'; ?>
+</header>
 <main>
     <h1>Blog</h1>
     <article>
--- portfolio/experience.php
+++ portfolio/experience.php
@@ -1,12 +1,28 @@
-<?php
-    // Define variables used in the HTML header for this page.
-    $title = 'Shaylyn Karan | Experience' ;
-    $description = 'Creating a portfolio for final assignment in CSCI 202' ;
-    $author = 'Tom Pollard' ;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    // Display the header which uses the above variables.
-    include 'includes/header.php' ;
-?>
+    <title>Experience | Shaylyn Karan></title>
+    <meta name="description" content="Personal Portfolio">
+    <meta name="author" content="Shaylyn Karan">
+
+    <!-- link to external CSS file -->
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+<header class="header">
+  <a href="index.php" class="logo">Shaylyn's Portfolio</a>
+  <input class="menu-btn" type="checkbox" id="menu-btn" />
+  <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+
+  <?php include 'includes/nav.php'; ?>
+</header>
 <body>
     <h1>Work Experience</h1>
     <h2>KCTS 9, Cascade Public Media</h2>
